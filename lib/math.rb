f74606a87de5273a0b1ea9935b87e1860629ba58
Nick Gray <nickgray87@gmail.com>
2021-01-14 01:07:43 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -3,21 +3,27 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-
+  sum =  num1 + num2
+  return sum
 end
 
 def division(num1, num2)
-
+  div = num1/num2
+  return div
 end
 
 def multiplication(num1, num2)
-
+  mult = num1*num2
+  return mult
 end
 
 def modulo(num1, num2)
+  mod = num1%num2
+  return mod
 
 end
 
 def square_root(num)
+  sr = 
 
 end
